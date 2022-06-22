88f5ec3f3e307e189fb0d53714e8305129f1845d
VictorEnache <victor22enache@gmail.com>
2022-06-22 07:18:32 +0200

    make seeder

---
+++ config/db.php
@@ -0,0 +1,25 @@
+<?php
+return [
+    'posts' => [
+        [
+            'title' => 'title 1',
+            'content' => 'Contenuto numero 1',
+            'image' => 'https://picsum.photos/200'
+        ],
+        [
+            'title' => 'title 2',
+            'content' => 'Contenuto numero 2',
+            'image' => 'https://picsum.photos/200'
+        ],
+        [
+            'title' => 'title 3',
+            'content' => 'Contenuto numero 3',
+            'image' => 'https://picsum.photos/200'
+        ],
+        [
+            'title' => 'title 4',
+            'content' => 'Contenuto numero 4',
+            'image' => 'https://picsum.photos/200'
+        ],
+    ]
+];
--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Database\Seeder;
 
 class PostSeeder extends Seeder
@@ -11,6 +12,14 @@ class PostSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $posts = config('db.posts');
+
+        foreach($posts as $post){
+            $new_post = new Post();
+            $new_post->title = $post['title'];
+            $new_post->content = $post['content'];
+            $new_post->image = $post['image'];
+            $new_post->save();
+        }
     }
 }
